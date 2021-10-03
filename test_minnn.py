0630260df8a2fbfbee95930ca3a22d85605a4de4
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2021-10-03 14:52:24 -0400

    changed asserts and moved to use of np testing

--- test_minnn.py
+++ test_minnn.py
@@ -2,14 +2,15 @@
 
 # a script to test the minnn.py
 # -- only with simple input/output examples
-# -- usage is simply: python test_minnn_old.py <your-minnn.py>
+# -- usage is simply: python test_minnn.py <your-minnn.py>
 
 import os
-import sys
 import unittest
 from unittest import TestCase
 
 import numpy as np
+
+import minnn as test_mn
 
 np.random.seed(123)
 
@@ -30,11 +31,7 @@ def load_module(f: str, m: str):
     return ret
 
 def is_allclose(x, y):
-    return np.allclose(x, y, rtol=1.e-3, atol=1.e-5)
-    # return np.abs(x-y).sum().item() <= 1e-5
-
-test_minnn = "minnn"
-test_mn = load_module(test_minnn, 'mn')
+    return np.testing.assert_allclose(x, y, rtol=1.e-3, atol=1.e-5)
 
 # --
 # helpers
@@ -48,7 +45,7 @@ class TestMinnn(TestCase):
         t = test_mn.astensor([0., 0.])
         t.accumulate_grad(g0)
         t.accumulate_grad(g1)
-        assert is_allclose(t.get_dense_grad(), np.asarray([4., 6.]))
+        is_allclose(t.get_dense_grad(), np.asarray([4., 6.]))
         # --
 
     def test_accumulate_grad_sparse(self):
@@ -56,16 +53,16 @@ class TestMinnn(TestCase):
         g1 = np.asarray([3., 4.])
         t = test_mn.astensor([[0., 0.], [0., 0.], [0., 0.]])
         t.accumulate_grad_sparse([(0, g0), (2, g1), (2, g0)])
-        assert is_allclose(t.get_dense_grad(), np.asarray([g0, [0., 0.], g0 + g1]))
+        is_allclose(t.get_dense_grad(), np.asarray([g0, [0., 0.], g0 + g1]))
         # --
 
     def test_xavier_uniform(self):
         shape = [100, 500]
         y = test_mn.Initializer.xavier_uniform(shape)
-        assert list(y.shape) == shape
-        assert abs(y.mean()) <= 1e-2
-        assert abs(y.max() - 0.1) <= 1e-2
-        assert abs(y.min() + 0.1) <= 1e-2
+        self.assertEqual(list(y.shape), shape)
+        self.assertLessEqual(abs(y.mean()), 1e-2)
+        self.assertLessEqual(abs(y.max() - 0.1), 1e-2)
+        self.assertLessEqual(abs(y.min() + 0.1), 1e-2)
         # --
 
     def test_momentum_update(self):
@@ -78,91 +75,60 @@ class TestMinnn(TestCase):
             g = np.array([1., 1.])
             p1.accumulate_grad(g)
             t1.update()
-        assert is_allclose(p1.data, np.array([-0.131441, -0.131441])), \
+        is_allclose(p1.data, np.array([-0.131441, -0.131441])), \
             "This is the values using our implementation, there can be other versions for momentum_update, you can choose to use others!"
         for i in range(6):
             g = np.array([1., 1.])
             p2.accumulate_grad_sparse([((i % 2), g.copy())])
             t1.update()
-        assert is_allclose(p2.data, np.array([[-0.1002459, -0.1002459], [-0.078051, -0.078051]])), \
+        is_allclose(p2.data, np.array([[-0.1002459, -0.1002459], [-0.078051, -0.078051]])), \
             "This is the values using our implementation, there can be other versions for momentum_update, you can choose to use others!"
         # --
 
     def test_lookup(self):
         t =  test_mn.astensor([[1., 2.], [3., 4.], [5., 6.]])
         v =  test_mn.lookup(t, [1, 0, 1])
-        assert is_allclose(v.data, np.asarray([[3., 4.], [1., 2.], [3., 4.]]))
+        is_allclose(v.data, np.asarray([[3., 4.], [1., 2.], [3., 4.]]))
         v.accumulate_grad(np.asarray([[1., 1.], [1., 1.], [2., 3.]]))
         v.op.backward()
-        assert is_allclose(t.get_dense_grad(), np.asarray([[1., 1.], [3., 4.], [0., 0.]]))
+        is_allclose(t.get_dense_grad(), np.asarray([[1., 1.], [3., 4.], [0., 0.]]))
 
     def test_dot(self):
-        w, h =  test_mn.astensor([[0., 1.], [2., 3.]]),  test_mn.astensor([1., 2.])
-        v =  test_mn.dot(w, h)
-        assert is_allclose(v.data, np.asarray([2., 8.]))
+        w, h = test_mn.astensor([[0., 1.], [2., 3.]]),  test_mn.astensor([1., 2.])
+        v = test_mn.dot(w, h)
+        is_allclose(v.data, np.asarray([2., 8.]))
         v.accumulate_grad(np.asarray([1., 3.]))
         v.op.backward()
-        assert is_allclose(w.get_dense_grad(), np.asarray([[1., 2.], [3., 6.]]))
-        assert is_allclose(h.get_dense_grad(), np.asarray([6., 10.]))
+        is_allclose(w.get_dense_grad(), np.asarray([[1., 2.], [3., 6.]]))
+        is_allclose(h.get_dense_grad(), np.asarray([6., 10.]))
 
     def test_tanh(self):
-        x =  test_mn.astensor([0., 1., 2., 3.])
-        v =  test_mn.tanh(x)
-        assert is_allclose(v.data, np.asarray([0., 0.76159416, 0.96402758, 0.99505475]))
+        x = test_mn.astensor([0., 1., 2., 3.])
+        v = test_mn.tanh(x)
+        is_allclose(v.data, np.asarray([0., 0.76159416, 0.96402758, 0.99505475]))
         v.accumulate_grad(np.asarray([1., 2., 3., 4.]))
         v.op.backward()
-        assert is_allclose(x.get_dense_grad(), np.asarray([1., 0.83994868, 0.21195247, 0.03946415]))
+        is_allclose(x.get_dense_grad(), np.asarray([1., 0.83994868, 0.21195247, 0.03946415]))
         # --
 
     def test_avg(self):
-        x =  test_mn.astensor([[0., 1., 2.], [3., 5., 11.]])
-        v =  test_mn.avg(x, 0)
-        assert is_allclose(v.data, np.asarray([1.5, 3., 6.5]))
+        x = test_mn.astensor([[0., 1., 2.], [3., 5., 11.]])
+        v = test_mn.avg(x, 0)
+        is_allclose(v.data, np.asarray([1.5, 3., 6.5]))
         v.accumulate_grad(np.asarray([1., 2., 3.]))
         v.op.backward()
-        assert is_allclose(x.get_dense_grad(), np.asarray([[0.5, 1.0, 1.5], [0.5, 1.0, 1.5]]))
+        is_allclose(x.get_dense_grad(), np.asarray([[0.5, 1.0, 1.5], [0.5, 1.0, 1.5]]))
         # --
 
     def test_max(self):
-        x =  test_mn.astensor([[0., 5., 2.], [-2., 6., 1.]])
-        v =  test_mn.max(x, 0)
-        assert is_allclose(v.data, np.asarray([0., 6., 2.]))
+        x = test_mn.astensor([[0., 5., 2.], [-2., 6., 1.]])
+        v = test_mn.max(x, 0)
+        is_allclose(v.data, np.asarray([0., 6., 2.]))
         v.accumulate_grad(np.asarray([1., 2., 3.]))
         v.op.backward()
-        assert is_allclose(x.get_dense_grad(), np.asarray([[1., 0., 3.], [0., 2., 0.]]))
+        is_allclose(x.get_dense_grad(), np.asarray([[1., 0., 3.], [0., 2., 0.]]))
         # --
 
-
-
-
-
-# --
-# testings!
-
-
-# --
-def main(minnn: str):
-    test_mn = load_module(minnn, 'mn')
-    # --
-    # note: to change weights in this table
-    test_table = [
-        ("accumulate_grad", 1.,),
-        ("accumulate_grad_sparse", 1.,),
-        ("xavier_uniform", 1.,),
-        ("momentum_update", 1.),
-        ("lookup", 1.),
-        ("dot", 1.),
-        ("tanh", 1.),
-        ("avg", 1.),
-        ("max", 1.),
-    ]
-    for name, weight in test_table:
-        test_f = globals()[f"test_{name}"]
-        test_f(test_mn)
-        print(f"Pass {name}!")
-    return
-
-# --
 # extra one to test acc
 def eval_acc(f1: str, f2: str):
     with open(f1) as fd:
@@ -173,8 +139,3 @@ def eval_acc(f1: str, f2: str):
     good = sum(a==b for a,b in zip(preds1, preds2))
     print(f"Acc = {good}/{len(preds1)}={good/len(preds1)}")
 # --
-
-if __name__ == '__main__':
-    # main("minnn.py")
-    test_minnn = "minnn.py"
-    unittest.main()
